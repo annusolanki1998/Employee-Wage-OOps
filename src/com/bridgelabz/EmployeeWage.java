c125423da2ff9bdeb34460fb33a68bd9346f80b6
Annu <annusinghraj1998@gmail.com>
2022-06-05 14:30:41 +0530

    ADDED : Employee wage computation by using class method

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -4,10 +4,10 @@ public class EmployeeWage {
     public static final int IS_PART_TIME = 1;
     public static final int IS_FUll_TIME = 2;
     public static final int EMP_RATE_PER_HOUR = 25;
-    public static final int NUM_OF_WORKING_DAYS = 20;
-    public static final int MAX_HRS_IN_MONTH = 100;
+    public static final int NUM_OF_WORKING_DAYS = 10;
+    public static final int MAX_HRS_IN_MONTH = 90;
 
-    public static void employeeWage() {
+    public static void computeEmployeeWage() {
         int totalEmpHrs = 0, totalEmpWage = 0, totalWorkingDays = 0;
 
         while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays <= NUM_OF_WORKING_DAYS) {
@@ -19,31 +19,30 @@ public class EmployeeWage {
 
             switch (empCheck) {
 
-                case IS_FUll_TIME:
+                case IS_FUll_TIME :
                     empHrs = 8;
                     break;
 
-                case IS_PART_TIME:
+                case IS_PART_TIME :
                     empHrs = 4;
                     break;
 
-                default:
+                default :
                     empHrs = 0;
             }
 
             totalEmpHrs += empHrs;
+            System.out.println("Working hours : " +empHrs);
             int empWage = empHrs * EMP_RATE_PER_HOUR;
-
             totalEmpWage += empWage;
-            System.out.println("Employee Wage : " + empWage);
+            System.out.println("Employee Wage : " +empWage);
         }
-
-        System.out.println("Total employee wage : " + totalEmpWage);
+        System.out.println("Total working hours : " +totalEmpHrs);
+        System.out.println("Total employee wage : " +totalEmpWage);
     }
 
     public static void main(String[] args) {
 
-        employeeWage();
+        computeEmployeeWage();
     }
-
 }
