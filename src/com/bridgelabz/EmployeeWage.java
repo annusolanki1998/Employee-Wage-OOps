74c516b6adf77dd7777e8fa60dae84d949a58642
Annu <annusinghraj1998@gmail.com>
2022-06-05 12:52:20 +0530

    ADDED : Calculate part time also.

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -1,24 +1,26 @@
 package com.bridgelabz;
 
 public class EmployeeWage {
-
-    public static void dailyEmployeeWage() {
+    public static void employeeWage() {
         int is_FULL_TIME = 1;
+        int is_PART_TIME = 2;
         int emp_RATE_PER_HOUR = 20;
         int empHrs = 0;
         int empWage = 0;
-        int empCheck = (int) Math.floor(Math.random() * 10) % 2;
+        int empCheck = (int) Math.floor(Math.random() * 10) % 3;
         if (empCheck == is_FULL_TIME)
             empHrs = 8;
+        else if (empCheck == is_PART_TIME)
+            empHrs = 4;
         else
             empHrs = 0;
 
         empWage = empHrs * emp_RATE_PER_HOUR;
-        System.out.println("The total employeeWage are : " + empWage);
+        System.out.println("The total employee wage are : " + empWage);
 
     }
 
     public static void main(String[] args) {
-        dailyEmployeeWage();
+        employeeWage();
     }
 }
