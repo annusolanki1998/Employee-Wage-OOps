aa3b8fb413a5606dbb976ea140276c3df771f28b
Annu <annusinghraj1998@gmail.com>
2022-06-05 14:19:04 +0530

    ADDED : Calculate the monthly wages till 100 Hours

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -1,40 +1,48 @@
 package com.bridgelabz;
 
 public class EmployeeWage {
-    public static final int IS_PART_TIME = 2;
-    public static final int IS_FULL_TIME = 1;
+    public static final int IS_PART_TIME = 1;
+    public static final int IS_FUll_TIME = 2;
     public static final int EMP_RATE_PER_HOUR = 25;
-    public static final int NUM_OF_WORKING_DAY = 20;
+    public static final int NUM_OF_WORKING_DAYS = 20;
+    public static final int MAX_HRS_IN_MONTH = 100;
 
     public static void employeeWage() {
-        int empHour = 0, empWage = 0, totalWage = 0;
+        int totalEmpHrs = 0, totalEmpWage = 0, totalWorkingDays = 0;
 
-        for (int i = 0; i < NUM_OF_WORKING_DAY; i++) {
+        while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays <= NUM_OF_WORKING_DAYS) {
+
+            int empHrs = 0;
+            totalWorkingDays++;
+
             int empCheck = (int) Math.floor(Math.random() * 10) % 3;
 
             switch (empCheck) {
-                case IS_FULL_TIME :
-                    empHour = 8;
+
+                case IS_FUll_TIME:
+                    empHrs = 8;
                     break;
 
-                case IS_PART_TIME:_TIME :
-                empHour = 4;
+                case IS_PART_TIME:
+                    empHrs = 4;
                     break;
 
                 default:
-                    empHour = 0;
+                    empHrs = 0;
             }
 
-            empWage = empHour * EMP_RATE_PER_HOUR;
-            totalWage += empWage;
+            totalEmpHrs += empHrs;
+            int empWage = empHrs * EMP_RATE_PER_HOUR;
 
-            System.out.println("Employee wages are : " + empWage);
+            totalEmpWage += empWage;
+            System.out.println("Employee Wage : " + empWage);
         }
 
-        System.out.println("The total employee wages are : " +totalWage);
+        System.out.println("Total employee wage : " + totalEmpWage);
     }
 
     public static void main(String[] args) {
+
         employeeWage();
     }
 
