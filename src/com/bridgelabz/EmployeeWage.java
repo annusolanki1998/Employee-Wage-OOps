4b2d0a72b68e2d517f9fd357fb0d0a9f88106903
Annu <annusinghraj1998@gmail.com>
2022-06-05 11:31:23 +0530

    ADDED : Welcome to employee wage computation !

---
+++ src/com/bridgelabz/EmployeeWage.java
@@ -0,0 +1,13 @@
+package com.bridgelabz;
+
+public class EmployeeWage {
+    public static void welcomeMessage() {
+        String name = "EmployeeWage";
+        System.out.println("Welcome to " + name + " Computation ! ");
+    }
+
+    public static void main(String[] args) {
+
+        welcomeMessage();
+    }
+}
