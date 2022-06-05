df2942970557f3eb7e17caf9ee126f1ebac143c3
Annu <annusinghraj1998@gmail.com>
2022-06-05 12:05:11 +0530

    ADDED : Check the employee is present or absent !

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -1,13 +1,16 @@
 package com.bridgelabz;
 
 public class EmployeeWage {
-    public static void welcomeMessage() {
-        String name = "EmployeeWage";
-        System.out.println("Welcome to " + name + " Computation ! ");
+    public static void empAttendance() {
+        int is_full_time = 1;
+        int empCheck = (int) Math.floor(Math.random() * 10) % 2;
+        if (empCheck == is_full_time)
+            System.out.println("Employee is present !");
+        else
+            System.out.println("Employee is absent ! ");
     }
 
     public static void main(String[] args) {
-
-        welcomeMessage();
+        empAttendance();
     }
 }
